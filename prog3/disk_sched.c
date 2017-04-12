962ac2b01bd02737900d728ee36d694883595608
Ben Kaiser <strmbrkr00@aol.com>
2017-04-12 12:51:14 -0600

    started fcfs

--- prog3/disk_sched.c
+++ prog3/disk_sched.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
-
+#include <time.h>
 
 void generateRequests()
 {
-
+    srand(
 }
 
 int main(int argc, char** argv)
--- prog3/fcfs.c
+++ prog3/fcfs.c
@@ -1,2 +1,11 @@
 #include "disk_sched.h"
 
+
+int fcfs()
+{
+    int totalDistance = 0;
+
+
+
+    return totalDistance;
+}
