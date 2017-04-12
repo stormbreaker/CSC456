b895a8b3ad7192d7a7480abc3fc46c9e236d5b8e
Ben Kaiser <strmbrkr00@aol.com>
2017-04-12 12:20:24 -0600

    began homework3

---
+++ prog3/disk_sched.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+
+void fcfs()
+{
+
+}
+
+void sstf()
+{
+
+}
+
+void scan()
+{
+
+}
+
+void cscan()
+{
+
+}
+
+void look()
+{
+
+}
+
+void clook()
+{
+
+}
+
+int main(int argc, char** argv)
+{
+
+}
